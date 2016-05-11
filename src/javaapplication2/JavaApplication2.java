21cedaaf7d15eacb44cf7e3ea8e9ccfbe600c74f
petr.huml <petr.huml@A211-05.pslib.cz>
2016-05-11 12:29:55 +0200

    tisk

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello world");
 // TODO code application logic here
     }
     
