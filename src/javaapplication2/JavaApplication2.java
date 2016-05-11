81196fd115819ab2a2467d812d3c96bb03d58a88
petr.huml <petr.huml@A211-05.pslib.cz>
2016-05-11 12:36:30 +0200

    tisk2

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");        
 // TODO code application logic here
     }
     
